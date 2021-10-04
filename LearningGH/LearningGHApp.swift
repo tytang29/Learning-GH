ff0c7130f91e3dcfa2fc6a9a119a038ec4552a94
Xavier Tang <xaviertang@yahoo.com.hk>
2021-10-04 17:15:33 +0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by tytang29 on 4/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
